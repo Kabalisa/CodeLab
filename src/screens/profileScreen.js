844ab5744e1ffb9eb32f4aff73c40c0a6c955842
Innocent Fiston Kabalisa <ikabalisa20@gmail.com>
2019-08-29 16:49:51 +0200

    feat(profile page): view a single developer's profile -list a developer's information when you click on their image or username [finishes #166258436]

--- src/screens/profileScreen.js
+++ src/screens/profileScreen.js
@@ -6,20 +6,30 @@ import userAvatar from '../../assets/userAvatar.png';
 class ProfileScreen extends Component {
   render() {
     const { navigation } = this.props;
+    const item = navigation.getParam('item');
     return (
       <View>
         <Header navigation={navigation} />
         <View style={styles.viewStyle}>
-          <Image source={userAvatar} style={styles.imageStyle} />
-          <Text style={styles.usernameStyle}>Username</Text>
+          <Image
+            source={{ uri: item.node.avatarUrl }}
+            style={styles.imageStyle}
+          />
+          <Text style={styles.usernameStyle}>{item.node.login}</Text>
           <Text style={styles.textOneStyle}>Github URL:</Text>
-          <Text style={styles.textTwoStyle}>www.github.com/users/username</Text>
+          <Text
+            style={styles.textTwoStyle}
+          >{`https://github.com/${item.node.login}`}</Text>
           <View style={styles.viewOneStyle}>
-            <Text style={styles.textThreeStyle}>5</Text>
+            <Text style={styles.textThreeStyle}>
+              {item.node.repositories.totalCount}
+            </Text>
             <Text style={styles.textFourStyle}>repos</Text>
           </View>
           <View style={styles.viewTwoStyle}>
-            <Text style={styles.textThreeStyle}>5</Text>
+            <Text style={styles.textThreeStyle}>
+              {item.node.starredRepositories.totalCount}
+            </Text>
             <Text style={styles.textFourStyle}>starring</Text>
           </View>
           <View style={styles.buttonStyle}>
@@ -41,6 +51,7 @@ const styles = StyleSheet.create({
   imageStyle: {
     width: 220,
     height: 220,
+    borderRadius: 110,
     marginVertical: 15
   },
   usernameStyle: {
